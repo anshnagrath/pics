bc9da6258e50d0f0eef81b1d80db6fd5b7734fcd
anshnagrath <anshnagrath448@gmail.com>
2019-04-14 17:09:42 +0530

    alligned properly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Searchbar from './Searchbar'
+import Images from './Images'
+import Unsplash from'./Unsplash'
 class App extends Component {
+
+  state={
+   images:[] 
+  };
+   onSearchSubmit=async(term)=>{
+let data = await Unsplash.get('/search/photos',{
+  params:{query:term},
+  headers:{
+    Authorization:"Client-ID ca83915ba196aa851cbd9d9e54b777093176d2c9c49ae40640d5ffb39388aa38"
+  },
+})
+console.log(data,'logging data')
+this.setState({images:data.data.results})
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="ui container">
+         <Searchbar onSubmit={this.onSearchSubmit}/>
+
+         <Images imgs={this.state.images} style={{marginTop:'20px'}}/>
+
       </div>
     );
   }
